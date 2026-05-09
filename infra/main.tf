88eff601be707fe29bf06ee9a6529201a703331b
Narayan Aryal <narayanaryal.code@gmail.com>
2026-05-09 19:11:47 +1000

    updated infra and deply.ymal

--- infra/main.tf
+++ infra/main.tf
@@ -1,9 +1,3 @@
-
-resource "aws_key_pair" "deployer" {
-  key_name   = "smart-notes-key"
-  public_key = file("~/.ssh/smart-notes-key.pub")
-}
-
 resource "aws_security_group" "backend_sg" {
   name = "smart-notes-sg"
 
@@ -40,41 +34,16 @@ data "aws_ami" "ubuntu" {
   }
 }
 
+# ----------------------------
+# EC2 INSTANCE (NO KEY PAIR)
+# ----------------------------
 resource "aws_instance" "backend" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = var.instance_type
-  key_name      = aws_key_pair.deployer.key_name
 
   vpc_security_group_ids = [
     aws_security_group.backend_sg.id
   ]
-
-  user_data = <<-EOF
-              #!/bin/bash
-              sudo apt update -y
-
-              # Install Docker
-              sudo apt install docker.io -y
-              sudo systemctl start docker
-              sudo systemctl enable docker
-
-              # Install Git
-              sudo apt install git -y
-
-              # Clone repo
-              cd /home/ubuntu
-              git clone https://github.com/Emprcode/cicd-tf-docker-aws.git
-
-              # Run backend
-              cd cicd-tf-docker-aws/backend
-
-              docker build -t smart-notes-backend .
-
-              docker run -d -p 8000:8000 \
-                --env-file .env \
-                --name smart-notes \
-                smart-notes-backend
-              EOF
 
   tags = {
     Name = "smart-notes-backend"
